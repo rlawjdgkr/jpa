fd4426d9e40ee8cd05ff0e2acc20ccc2bd37fc3a
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-04 23:29:41 +0900

    2-4

--- src/main/java/com/study/jpa/chap01/entity/Product.java
+++ src/main/java/com/study/jpa/chap01/entity/Product.java
@@ -1,13 +1,14 @@
 package com.study.jpa.chap01.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Getter
-@Setter
+//@Setter
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
@@ -20,12 +21,36 @@ public class Product {
 
     // PK 설정
     @Id
-    @Column(name ="prod_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    @Column(name = "prod_id")
     private Long id;
 
-    @Column(name = "prod_nm" , nullable = false, length =  30)
+    @Column(name = "prod_nm", nullable = false, length = 30)
     private String name; // 상품명
 
-    @Column(name = "prod_price" , nullable = true)
+    @Column(name = "prod_price", nullable = true)
     private int price; // 상품 가격
+
+    @CreationTimestamp   // INSERT시 자동으로 시간 저장
+    @Column(updatable = false) // 수정 불가
+    private LocalDateTime createdAt; // 상품 등록 시간
+
+    @UpdateTimestamp   // UPDATE문 실행 시 자동으로 시간 수정
+    private LocalDateTime updatedAt; // 상품 정보 수정 시간
+
+    // 열거형 데이터는 따로 설정을 안하면 숫자로 저장됨
+    @Enumerated(EnumType.STRING)
+    private Category category; // 상품 카테고리
+
+    public enum Category {
+        FOOD, FASHION, ELECTRONIC
+    }
+
+    // 수정 편의 메서드
+    public void changeProduct(String newName, int newPrice, Category newCategory) {
+        this.name = newName;
+        this.price = newPrice;
+        this.category = newCategory;
+    }
+
 }
